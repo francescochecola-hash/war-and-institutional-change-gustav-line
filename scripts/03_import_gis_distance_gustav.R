fbe411aff5be8b9f8d166ecfac559a3a2dd1b90e
Francesco Checola <francesco.checola@unito.it>
2026-02-08 01:00:27 +0100

    Create 03_import_gis_distance_gustav.R

---
+++ scripts/03_import_gis_distance_gustav.R
@@ -0,0 +1,10 @@
+# ==============================================================================
+# Francesco Checola
+# War and Institutional Change: The Case of Gustav Line
+#
+# Script: 03_import_gis_distance_gustav.R
+# Purpose: Import raw data (.txt) and save them
+#          as R versions (.rds) in the folder "data/processed/gis"
+# Output:  data/processed/gis/.rds
+# Notes:   Raw data are intentionally excluded from the repository (see .gitignore)
+# ==============================================================================
