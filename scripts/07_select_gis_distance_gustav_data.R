b32b870707e5f2f0329d71384d44fccd8287bd7f
Francesco Checola <francesco.checola@unito.it>
2026-02-10 11:35:45 +0100

    Create 07_select_gis_distance_gustav_data.R

---
+++ scripts/07_select_gis_distance_gustav_data.R
@@ -0,0 +1,12 @@
+# ==============================================================================
+# Francesco Checola
+# War and Institutional Change: The Case of Gustav Line
+#
+# Script: 07_select_gis_distance_gustav_data.R
+# Purpose: Select relevant variables from datasetn with municipality-level
+#          observations distance from Gustav Line 
+#          replication datasets for subsequent analysis 
+# Output: data/processed/gis_gustav_distance_selected.rds
+# Notes: Variable selection reflects empirical specifications used in the thesis
+# ==============================================================================
+
