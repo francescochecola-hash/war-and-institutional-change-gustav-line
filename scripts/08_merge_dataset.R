ed2393e58602e2956c4c481e8d2e22e0961296ec
Francesco Checola <francesco.checola@unito.it>
2026-02-11 13:33:46 +0100

    Create 08_merge_dataset.R script

---
+++ scripts/08_merge_dataset.R
@@ -0,0 +1,13 @@
+# ==============================================================================
+# Francesco Checola
+# War and Institutional Change: The Case of Gustav Line
+#
+# Script: 08_merge_dataset.R
+# Purpose: 
+# Inputs: data/processed/select/.rds
+#         data/processed/select/.rds
+#         data/processed/select/.rds
+#         data/processed/select/.rds
+# Output: data/processed/merge/gustav_line_dataset.rds
+# Notes: 
+# ==============================================================================
